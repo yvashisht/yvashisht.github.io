e34db9fe661755a0f55178a663193c08e5fd5d07
Yajur Vashisht <132369766+yvashisht@users.noreply.github.com>
2023-12-27 17:16:29 -0700

    Touch ups

--- website/src/sections/about/data.js
+++
@@ -1,9 +0,0 @@
-import { FaAward } from 'react-icons/fa'
-import { GoProjectSymlink } from "react-icons/go";
-
-const data = [
-    { id: 1, icon: <FaAward />, title: 'Experience', desc: '3+ Years' },
-    { id: 2, icon: <GoProjectSymlink />, title: 'Projects', desc: 'Comprehensive Project Experience' },
-]
-
-export default data;
